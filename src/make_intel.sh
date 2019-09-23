44b7866fed69926b3a4934fbc32c45571f0fceb4
Zeli Tan <tanzeli1982@gmail.com>
2019-09-23 13:06:19 -0700

    Update make_intel.sh

--- src/make_intel.sh
+++ src/make_intel.sh
@@ -2,8 +2,10 @@
 
 arg=$( echo $1 | tr '[:upper:]' '[:lower:]' )
 if [ -z "$arg" ]; then
-   ifort -O3 -fast -c -fPIC data_buffer_mod.f90 hydro_utilities_mod.f90 
-   f2py -c --quiet --fcompiler=intelem --opt='-O3 -fast' -I. data_buffer_mod.o hydro_utilities_mod.o -m TAIHydroMOD tai_hydro_mod.f90 
+   #ifort -O3 -fast -c -fPIC data_buffer_mod.f90 hydro_utilities_mod.f90
+   #f2py -c --quiet --fcompiler=intelem --opt='-O3 -fast' -I. data_buffer_mod.o hydro_utilities_mod.o -m TAIHydroMOD tai_hydro_mod.f90
+   ifort -O3 -c -fPIC data_buffer_mod.f90 hydro_utilities_mod.f90 
+   f2py -c --quiet --fcompiler=intelem --opt='-O3' -I. data_buffer_mod.o hydro_utilities_mod.o -m TAIHydroMOD tai_hydro_mod.f90 
 elif [ $arg = 'clean' ]; then
    sources=$( ls *.f90 )
    objects=$( find . -type f \( -name \$sources -o -name \*.o \) )
