0c4ce7e17594bde97a2387f50a568c14b282a618
Zeli Tan <tanzeli1982@gmail.com>
2020-01-07 13:58:37 -0800

    Update example_flow_velocity.py

--- scripts/example_flow_velocity.py
+++ scripts/example_flow_velocity.py
@@ -10,16 +10,20 @@ Draw the dynamics of water flow velocity on the TAI platform
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.io import netcdf
+from netCDF4 import Dataset
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 # read the simulated water flow velocity
 filename = '/Users/tanz151/Python_maces/src/out_hydro_2001-01-01_2001-01-06.nc'
 try:
-    nc = netcdf.netcdf_file(filename,'r')
+    nc = Dataset(filename,'r')
     xv = np.array(nc.variables['x'][:][0])
     Uw = np.array(nc.variables['U'][:][0])
 finally:
     nc.close()
+ntime = np.shape(Uw)[0]
+
+tt = -np.arange(ntime)
     
 # plot
 plt.clf()
@@ -27,3 +31,23 @@ fig, ax = plt.subplots(figsize=(8,10))
 
 plt.style.use('default')
 
+cf = ax.contourf(xv, tt, Uw, 10, cmap='RdYlBu_r')
+ax.set_xlim([xv[0], xv[-1]])
+ax.set_ylim([tt[-1], tt[0]])
+cbar = fig.colorbar(cf, ax=ax, orientation='horizontal', pad=0.05)
+ylabel = 'Water flow ($\mathregular{m}$ $\mathregular{{s}^{-1}}$)'
+cbar.set_label(ylabel, fontsize=12, fontname='Times New Roman', 
+               labelpad=-50)
+#cbar.ax.xaxis.set_minor_locator(AutoMinorLocator(2))
+labels = cbar.ax.get_xticklabels()
+[label.set_fontname('Times New Roman') for label in labels]
+[label.set_fontsize(12) for label in labels]
+
+labels = ax.get_xticklabels() + ax.get_yticklabels()
+[label.set_fontname('Times New Roman') for label in labels]
+[label.set_fontsize(12) for label in labels]
+
+plt.tight_layout()
+fig.savefig('flow.png', dpi=300)
+#fig.savefig('F1.pdf', dpi=600)
+plt.show()
