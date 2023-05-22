d6c61c2b2086f1058f0d17852bfac898c99b4086
Zeli Tan <tanzeli1982@gmail.com>
2023-05-21 22:00:10 -0700

    Update draw_plum_sed_site.py

--- scripts/draw_plum_sed_site.py
+++ scripts/draw_plum_sed_site.py
@@ -21,10 +21,7 @@ z_marsh = 1.686         # marsh elevation
 day0 = (date(2017,7,19) - date(2017,7,17)).days
 day1 = (date(2017,7,23) - date(2017,7,17)).days
 
-models = ['F06', 'T03', 'KM12']#, 'M12', 'F07', 'VDK05', 'DA07']
-min_accr_sim = {}
-sed_sim_c = {}
-sed_sim_m = {}
+models = ['F06', 'T03', 'KM12', 'M12']#, 'F07', 'VDK05', 'DA07']
 
 # read simulation outputs
 rdir = '/Users/tanz151/Documents/Projects/TAI_BGC/Drafts/Outputs/PlumIsland/'
@@ -38,6 +35,7 @@ finally:
 index0 = np.argmin(np.abs(zh))
 x = x - x[index0]
 
+min_accr_sim = {}
 for model in models:
     filename = rdir + 'maces_ecogeom_2017-07-17_2017-08-01_' + model + '_4097.nc'
     try:
@@ -51,17 +49,38 @@ for model in models:
 # find the site index
 index1 = np.argmin(np.abs(zh - z_channel))
 index2 = np.argmin(np.abs(zh - z_marsh))
+print(index1, index2)
 
+sed_sim_c = {}
+sed_sim_m = {}
+U_sim_c = {}
+U_sim_m = {}
+Uwav_sim_c = {}
+Uwav_sim_m = {}
+tau_sim_c = {}
+tau_sim_m = {}
 for model in models:
     filename = rdir + 'maces_hydro_2017-07-17_2017-08-01_' + model + '_4097.nc'
     try:
         nc = Dataset(filename,'r')
         sed_c = np.array(nc.variables['TSM'][day0*24:day1*24,index1])
         sed_m = np.array(nc.variables['TSM'][day0*24:day1*24,index2])
+        U_c = np.array(nc.variables['U'][day0*24:day1*24,index1])
+        U_m = np.array(nc.variables['U'][day0*24:day1*24,index2])
+        Uwav_c = np.array(nc.variables['Uwav'][day0*24:day1*24,index1])
+        Uwav_m = np.array(nc.variables['Uwav'][day0*24:day1*24,index2])
+        tau_c = np.array(nc.variables['tau'][day0*24:day1*24,index1])
+        tau_m = np.array(nc.variables['tau'][day0*24:day1*24,index2])
     finally:
         nc.close()
     sed_sim_c[model] = 1e3 * np.reshape(sed_c,(24*(day1-day0)))    # mg/L
     sed_sim_m[model] = 1e3 * np.reshape(sed_m,(24*(day1-day0)))    # mg/L
+    U_sim_c[model] = 100*np.reshape(U_c,(24*(day1-day0)))
+    U_sim_m[model] = 100*np.reshape(U_m,(24*(day1-day0)))
+    Uwav_sim_c[model] = 100*np.reshape(Uwav_c,(24*(day1-day0)))
+    Uwav_sim_m[model] = 100*np.reshape(Uwav_m,(24*(day1-day0)))
+    tau_sim_c[model] = 1e3*np.reshape(tau_c,(24*(day1-day0)))
+    tau_sim_m[model] = 1e3*np.reshape(tau_m,(24*(day1-day0)))
 
 nt_model = np.size(sed_sim_c['F06'])
 tt_model = np.arange(nt_model)
@@ -109,19 +128,35 @@ for key in sed_sim_c:
     h, = ax.plot(tt_model, sed_sim_c[key], color=colors[indx], 
                  linestyle=linestyles[indx], linewidth=2, alpha=1)
     handles.append(h)
+    rmse = 0.0
+    count = 0
+    for ii, tt in enumerate(tt_obs):
+        if tt in tt_model and np.isfinite(sed_obs_c[ii]):
+            index = np.where(tt_model==tt)
+            rmse = rmse + (sed_sim_c[key][index] - sed_obs_c[ii])**2
+            count = count + 1
+    rmse = np.sqrt( rmse / count )
+    nrmse = rmse / np.nanmean(sed_obs_c)
+    print(key, ': ', rmse, nrmse)
+#ax.plot(tt_model, U_sim_c['M12'], color='gray', linestyle='-', linewidth=1, alpha=0.9)
+#ax.plot(tt_model, Uwav_sim_c['M12'], color='gray', linestyle=':', linewidth=1, alpha=0.9)
+    ax.plot(tt_model, tau_sim_c['M12'], color='gray', linestyle='-', linewidth=1, alpha=0.9)
 ax.set_xlim(0, nt_model)
 ax.set_ylim(0, 50)
 ax.xaxis.set_ticks(np.arange(0,nt_model+1,24))
 ax.yaxis.set_ticks(np.linspace(0,50,6))
 ax.set_xticklabels(['7/19','7/20','7/21','7/22','7/23'])
 ax.xaxis.set_minor_locator(AutoMinorLocator(4))
-ax.set_xlabel('Time', fontsize=12, fontname='Times New Roman', color='black')
+ax.set_xlabel('Time', fontsize=12, fontname='Times New Roman', color='black', 
+              fontweight='bold')
 ylabel = 'Suspended sediment ($\mathregular{mg}$ $\mathregular{l^{-1}}$)'
-ax.set_ylabel(ylabel, fontsize=12, fontname='Times New Roman', color='black')
+ax.set_ylabel(ylabel, fontsize=12, fontname='Times New Roman', color='black', 
+              fontweight='bold')
 labels = ax.get_xticklabels() + ax.get_yticklabels()
 [label.set_fontname('Times New Roman') for label in labels]
 [label.set_fontsize(12) for label in labels]
 [label.set_color('black') for label in labels]
+[label.set_fontweight('bold') for label in labels]
 ax.text(0.05, 0.93, 'a', transform=ax.transAxes, fontsize=16,
         fontname='Times New Roman', fontweight='bold')
 ax.tick_params(which='major', direction='in', colors='xkcd:black', length=6, pad=8)
@@ -137,6 +172,19 @@ for key in sed_sim_m:
     h, = ax.plot(tt_model, sed_sim_m[key], color=colors[indx], 
                  linestyle=linestyles[indx], linewidth=2, alpha=1)
     handles.append(h)
+    rmse = 0.0
+    count = 0
+    for ii, tt in enumerate(tt_obs):
+        if tt in tt_model and np.isfinite(sed_obs_m[ii]):
+            index = np.where(tt_model==tt)
+            rmse = rmse + (sed_sim_m[key][index] - sed_obs_m[ii])**2
+            count = count + 1
+    rmse = np.sqrt( rmse / count )
+    nrmse = rmse / np.nanmean(sed_obs_m)
+    print(key, ': ', rmse, nrmse)
+#ax.plot(tt_model, U_sim_m['M12'], color='gray', linestyle='-', linewidth=1, alpha=0.9)
+#ax.plot(tt_model, Uwav_sim_m['M12'], color='gray', linestyle=':', linewidth=1, alpha=0.9)
+ax.plot(tt_model, tau_sim_m['M12'], color='gray', linestyle='-', linewidth=1, alpha=0.9)
 legend = ax.legend(handles, list(sed_sim_c.keys()), numpoints=1, loc=1, 
                    prop={'family':'Times New Roman', 'size':'large'}, 
                    framealpha=0.0)
@@ -146,13 +194,15 @@ ax.xaxis.set_ticks(np.arange(0,nt_model+1,24))
 ax.yaxis.set_ticks(np.linspace(0,20,6))
 ax.set_xticklabels(['7/19','7/20','7/21','7/22','7/23'])
 ax.xaxis.set_minor_locator(AutoMinorLocator(4))
-ax.set_xlabel('Time', fontsize=12, fontname='Times New Roman', color='black')
+ax.set_xlabel('Time', fontsize=12, fontname='Times New Roman', color='black', 
+              fontweight='bold')
 ylabel = 'Suspended sediment ($\mathregular{mg}$ $\mathregular{l^{-1}}$)'
 #ax.set_ylabel(ylabel, fontsize=12, fontname='Times New Roman', color='black')
 labels = ax.get_xticklabels() + ax.get_yticklabels()
 [label.set_fontname('Times New Roman') for label in labels]
 [label.set_fontsize(12) for label in labels]
 [label.set_color('black') for label in labels]
+[label.set_fontweight('bold') for label in labels]
 ax.text(0.05, 0.93, 'b', transform=ax.transAxes, fontsize=16,
         fontname='Times New Roman', fontweight='bold')
 ax.tick_params(which='major', direction='in', colors='xkcd:black', length=6, pad=8)
