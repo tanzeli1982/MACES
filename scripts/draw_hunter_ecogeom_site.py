Correct the calculation of min and om accretion

scripts/draw_hunter_ecogeom_site.py
@@ -77,17 +77,18 @@ zh_ref[zh_ref>=0] = -1e20
 index_obs.append(np.argmax(zh_ref))
 print("index_obs: ", index_obs)
 
+nyear = 1.0
 for model in min_models:
     for omodel in om_models:
         filename = rdir + 'maces_ecogeom_2004-01-01_2005-01-01_' + model + \
             '%' + omodel + '_11099.nc'
         try:
             nc = Dataset(filename,'r')
-            Esed = 8.64e7*np.sum(np.array(nc.variables['Esed'][:]),axis=0)  # kg/m2/yr
-            Dsed = 8.64e7*np.sum(np.array(nc.variables['Dsed'][:]),axis=0)  # kg/m2/yr
+            Esed = 8.64e7*np.sum(np.array(nc.variables['Esed'][:]),axis=0)/nyear  # g/m2/yr
+            Dsed = 8.64e7*np.sum(np.array(nc.variables['Dsed'][:]),axis=0)/nyear  # g/m2/yr
             Bag = 1e3*np.mean(np.array(nc.variables['Bag'][day0:day1,:]),axis=0)    # g/m2
             Bmax = 1e3*np.max(np.array(nc.variables['Bag'][day0:day1,:]),axis=0)    # g/m2
-            om_accr = 8.64e7*np.sum(np.array(nc.variables['DepOM'][:]),axis=0)  # g/m2/yr
+            om_accr = 8.64e7*np.sum(np.array(nc.variables['DepOM'][:]),axis=0)/nyear  # g/m2/yr
         finally:
             nc.close()
         minac_sim = (np.interp(site_dem, zh, Dsed) - np.interp(site_dem, zh, Esed)) / \

scripts/draw_plum_ecogeom_site.py
@@ -20,7 +20,7 @@ from datetime import date
 # read sediment density and porosity of different mineral accretion models
 min_models = ['F06', 'T03', 'KM12', 'M12', 'F07', 'VDK05', 'DA07']
 om_models = ['M12', 'DA07', 'KM12', 'K16']
-case_min = 'F07'
+case_min = 'DA07'
 case_om = 'M12'
 
 rhoSed = {}
@@ -74,15 +74,16 @@ nx = len(x)
 om_accr_sim = {}
 min_accr_sim = {}
 tot_accr_sim = {}
+nyear = 2.0
 for model in min_models:
     for omodel in om_models:
         filename = rdir + 'maces_ecogeom_2017-01-01_2019-01-01_' + model + \
             '%' + omodel + '_4097.nc'
         try:
             nc = Dataset(filename,'r')
-            Esed = 8.64e7*np.sum(np.array(nc.variables['Esed'][:]),axis=0)  # kg/m2/yr
-            Dsed = 8.64e7*np.sum(np.array(nc.variables['Dsed'][:]),axis=0)  # kg/m2/yr
-            om_accr = 8.64e7*np.sum(np.array(nc.variables['DepOM'][:]),axis=0)  # g/m2/yr
+            Esed = 8.64e7*np.sum(np.array(nc.variables['Esed'][:]),axis=0)/nyear  # g/m2/yr
+            Dsed = 8.64e7*np.sum(np.array(nc.variables['Dsed'][:]),axis=0)/nyear  # g/m2/yr
+            om_accr = 8.64e7*np.sum(np.array(nc.variables['DepOM'][:]),axis=0)/nyear  # g/m2/yr
         finally:
             nc.close()
         minac_sim_LAC = (np.interp(x_LAC, x, Dsed) - np.interp(x_LAC, x, Esed)) / \
@@ -264,9 +265,9 @@ legend = ax.legend(handles, om_models, numpoints=1, loc='upper center',
                    prop={'family':'Times New Roman', 'size':'large', 'weight': 'bold'},
                    framealpha=0.0)
 ax.set_xlim(0, 200)
-ax.set_ylim(50, 250)
+ax.set_ylim(0, 200)
 ax.xaxis.set_ticks(np.linspace(0,200,5))
-ax.yaxis.set_ticks(np.linspace(50,250,5))
+ax.yaxis.set_ticks(np.linspace(0,200,5))
 ax.xaxis.set_minor_locator(AutoMinorLocator(5))
 ax.set_xlabel('Distance ($\mathregular{m}$)', fontsize=12,
               fontname='Times New Roman', color='black', fontweight='bold')

scripts/draw_venice_ecogeom_site.py
@@ -78,17 +78,18 @@ x_ref[x_ref>=0] = -1e20
 index_obs.append(np.argmax(x_ref))
 print("index_obs: ", index_obs)
 
+nyear = 2.0
 for model in min_models:
     for omodel in om_models:
         filename = rdir + 'maces_ecogeom_2002-01-01_2004-01-01_' + model + \
             '%' + omodel + '_466.nc'
         try:
             nc = Dataset(filename,'r')
-            Esed = 8.64e7*np.sum(np.array(nc.variables['Esed'][:]),axis=0)  # kg/m2/yr
-            Dsed = 8.64e7*np.sum(np.array(nc.variables['Dsed'][:]),axis=0)  # kg/m2/yr
+            Esed = 8.64e7*np.sum(np.array(nc.variables['Esed'][:]),axis=0)/nyear  # g/m2/yr
+            Dsed = 8.64e7*np.sum(np.array(nc.variables['Dsed'][:]),axis=0)/nyear  # g/m2/yr
             Bag = 1e3*np.mean(np.array(nc.variables['Bag'][day0:day1,:]),axis=0)    # g/m2
             Bmax = 1e3*np.max(np.array(nc.variables['Bag'][day0:day1,:]),axis=0)    # g/m2
-            om_accr = 8.64e7*np.sum(np.array(nc.variables['DepOM'][:]),axis=0)  # g/m2/yr
+            om_accr = 8.64e7*np.sum(np.array(nc.variables['DepOM'][:]),axis=0)/nyear  # g/m2/yr
         finally:
             nc.close()
         minac_sim = (np.interp(0.2, x, Dsed) - np.interp(0.2, x, Esed)) / \